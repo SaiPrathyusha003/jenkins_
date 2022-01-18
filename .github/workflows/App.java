fa7a23c0d59b4a2933ef48b6a91da95feac873fd
SaiPrathyusha003 <55518779+SaiPrathyusha003@users.noreply.github.com>
2022-01-18 15:53:03 +0530

    Create App.java

---
+++ .github/workflows/App.java
@@ -0,0 +1,9 @@
+public class App
+{
+  public static void main(String args[])
+  {
+    DeepThought ourSuperComputer = new DeepThought();
+    int ans = ourSuperComputer.answer();
+    System.out.println(ans);
+  }
+}
