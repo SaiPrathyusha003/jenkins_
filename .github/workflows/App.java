696690efd716fc286c5d65f1e87509eaee51b6e5
SaiPrathyusha003 <55518779+SaiPrathyusha003@users.noreply.github.com>
2022-01-24 09:19:23 +0530

    Delete App.java

--- .github/workflows/App.java
+++
@@ -1,9 +0,0 @@
-public class App
-{
-  public static void main(String args[])
-  {
-    DeepThought ourSuperComputer = new DeepThought();
-    int ans = ourSuperComputer.answer();
-    System.out.println(ans);
-  }
-}
